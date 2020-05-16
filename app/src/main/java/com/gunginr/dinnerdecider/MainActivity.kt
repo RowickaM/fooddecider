3e99fb800aeeaca661fb23f2e4977659954e9f36
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-05-16 23:00:27 +0200

    init commit

---
+++ app/src/main/java/com/gunginr/dinnerdecider/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gunginr.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
