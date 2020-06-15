cef77a9cb499e318dd7a2d146fb4936449b7fa20
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-06-15 14:06:58 +0200

    add loading

--- app/src/main/java/com/gunginr/dinnerdecider/view/activity/authorization/LoginActivity.kt
+++ app/src/main/java/com/gunginr/dinnerdecider/view/activity/authorization/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.gunginr.dinnerdecider.view.activity.authorization
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.gunginr.dinnerdecider.R
 import com.gunginr.dinnerdecider.services.firebase.FirebaseAuthorisation
 import com.gunginr.dinnerdecider.util.goTo
 import com.gunginr.dinnerdecider.util.goToAndCloseOther
 import com.gunginr.dinnerdecider.view.activity.DecideActivity
+import com.gunginr.dinnerdecider.view.fragment.LoadingFragment
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -15,11 +17,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        showPopup(false)
         if (FirebaseAuthorisation.isAuth()) {
             this.goToAndCloseOther(DecideActivity::class.java)
         }
 
         login.setOnClickListener {
+            showPopup(true)
             FirebaseAuthorisation.singIn(
                 this,
                 emailInput.text.toString(),
@@ -31,8 +35,23 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun showPopup(show: Boolean) {
+        if (show) {
+            popup.visibility = View.VISIBLE
+            popup.setOnClickListener { }
+            popup.bringToFront()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, LoadingFragment())
+                .commit()
+        } else {
+            popup.visibility = View.GONE
+        }
+    }
+
     override fun onBackPressed() {
-        super.onBackPressed()
-        finishAffinity()
+        if (popup.visibility == View.GONE) {
+            finishAffinity()
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/gunginr/dinnerdecider/view/activity/authorization/RegistrationActivity.kt
+++ app/src/main/java/com/gunginr/dinnerdecider/view/activity/authorization/RegistrationActivity.kt
@@ -1,10 +1,12 @@
 package com.gunginr.dinnerdecider.view.activity.authorization
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.gunginr.dinnerdecider.R
 import com.gunginr.dinnerdecider.services.firebase.FirebaseAuthorisation
 import com.gunginr.dinnerdecider.util.goTo
+import com.gunginr.dinnerdecider.view.fragment.LoadingFragment
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
@@ -14,6 +16,7 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
 
         signup.setOnClickListener {
+            showPopup(true)
             FirebaseAuthorisation.singUp(
                 this,
                 emailInput.text.toString(),
@@ -23,6 +26,27 @@ class RegistrationActivity : AppCompatActivity() {
         }
         toLogin.setOnClickListener {
             this.goTo(LoginActivity::class.java)
+        }
+    }
+
+    private fun showPopup(show: Boolean) {
+        if (show) {
+            popup.visibility = View.VISIBLE
+            popup.setOnClickListener { }
+            popup.bringToFront()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, LoadingFragment())
+                .commit()
+        } else {
+            popup.visibility = View.GONE
+        }
+    }
+
+
+    override fun onBackPressed() {
+        if (popup.visibility == View.GONE) {
+            finishAffinity()
+            super.onBackPressed()
         }
     }
 }
